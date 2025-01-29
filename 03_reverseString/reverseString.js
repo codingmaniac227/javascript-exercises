bd1e3cd4cfd5b765c018d2b9d5a8d7f0acbabb8c
Marquise Davis <codingmaniac227@gmail.com>
2025-01-28 22:24:55 -0500

    created a string reverse function for unit testing

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let reversed = ''; // Establishing a variable to throw letters into new string called 'reversed'
 
+  for (let i = string.length - 1; i >= 0; i--) { // decrementing the the string from the end of the length of the string
+    reversed += string[i]; // appending the string pointer value to the variable 'reversed'
+  }             
+
+  return reversed; // returning my new reversed string
 };
 
 // Do not edit below this line
