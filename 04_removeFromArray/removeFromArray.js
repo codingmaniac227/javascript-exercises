da905062e31a49991b1c9f4e121afd604ee84d1f
Marquise Davis <codingmaniac227@gmail.com>
2025-01-28 23:42:30 -0500

    added function for test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...args) {
+    // Create a new array to hold elements that should not be removed
+    const newArray = [];
+  
+    // Loop through each element in the array
+    array.forEach((item) => {
+      // Check if the item is not in the args list
+      if (!args.includes(item)) {
+        // If the item is not in args, add it to the new array
+        newArray.push(item);
+      }
+    });
+  
+    // Return the new array with the items removed
+    return newArray;
+  };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
