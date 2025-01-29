d0fbd5ffb25adeb07547cda3aa47b47b99fce467
Marquise Davis <codingmaniac227@gmail.com>
2025-01-29 15:07:00 -0500

    redid the implementation for retention

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,20 +1,15 @@
-const removeFromArray = function(array, ...args) {
-    // Create a new array to hold elements that should not be removed
-    const newArray = [];
-  
-    // Loop through each element in the array
-    array.forEach((item) => {
-      // Check if the item is not in the args list
-      if (!args.includes(item)) {
-        // If the item is not in args, add it to the new array
-        newArray.push(item);
-      }
-    });
-  
-    // Return the new array with the items removed
-    return newArray;
-  };
 
+const removeFromArray = function(array, ...args) {
+  let newArray = [];
+
+  array.forEach((num) => {
+    if(!args.includes(num))
+      newArray.push(num);
+  });
+  return newArray;
+};
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
