5783f9e2f24175ae41c3462debabf502ada8ef45
Marquise Davis <codingmaniac227@gmail.com>
2025-01-29 17:51:20 -0500

    added function test cases for the sumAll.js file

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,29 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+
+    let sum = 0;
+
+    if (int1 < 0 || int2 < 0) {
+        return 'ERROR';
+
+    } else if ((!Number.isInteger(int1)) || (!Number.isInteger(int2))) {
+        return 'ERROR';
+
+    } else if ((typeof int1 !== "number") || (typeof int2 !== "number")) {
+        return 'ERROR';
+    
+    } else if ((int1 > int2) && (int1 && int2 >= 0)) {
+        for (let i = int1; i >= int2; i--) {
+            sum += i;
+        };
+
+    } else if ((int1 < int2) && (int1 && int2 >= 0)) {
+        for (let i = 0; i <= int2; i++) {
+            sum += i;
+        };
+
+    };
+
+    return sum;
 
 };
 
